d99a2e3f449cb78232c5d13a714314347ce3ee5b
Simhadri Kuppala <darlingmr39@gmail.com>
2026-02-22 21:31:06 +0530

    updated final frontrnd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://privacy-mining-project.onrender.com',
         changeOrigin: true,
         secure: false,
       }
